dfcabc59e9a9fcd3ec6948336209e8ad7ca1d336
Cheyna22 <humphries.c22@gmail.com>
2018-11-27 20:57:51 -0800

    started function to take in array/value

---
+++ src/main/java/ArrayShift.java
@@ -0,0 +1,22 @@
+public class ArrayShift{
+
+    public static String insertShiftArray(String[] num){
+        int[] shiftedArray = new int[5]; // create new array to help manipulate
+        int value = 5; // store value in variable to be used later
+        originalArray[0] = 1;
+        originalArray[1] = 2;
+        originalArray[2] = 3;
+        originalArray[3] = 4;
+
+        for(int i = 0; i < ans.length/2; i++) {
+            int[] shiftedArray = new originalArray.length +1 ;
+            shiftedArray[i] =
+        }
+
+        return ;
+    }
+
+    public static void main(){
+
+    }
+}
